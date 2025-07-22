499f25596517b605f2dd70a3df8dd1431cef6609
Giuseppe Vigneri <geppe1990@gmail.com>
2025-07-22 14:13:54 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	safelist: [{ pattern: /bg-my-dark-(success|danger|warning)/}],
 	theme: {
 		extend: {
 			colors: {
