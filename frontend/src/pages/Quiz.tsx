0020ea26810cae3757ac7efefaa97f6d97360c74
Giuseppe Vigneri <geppe1990@gmail.com>
2025-07-24 12:14:03 +0200

    Update Quiz.tsx

--- frontend/src/pages/Quiz.tsx
+++ frontend/src/pages/Quiz.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import { useState } from "react"
 import questions from "../domande.json"
 import { ChevronLeft } from "lucide-react"
 import { Link } from "react-router-dom"
