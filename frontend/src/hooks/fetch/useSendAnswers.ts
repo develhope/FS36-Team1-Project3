ca9761191f514d3e6ff8c11783689425c79c678f
sandro997 <alessandrofavazza97@gmail.com>
2025-07-28 01:24:55 +0200

    fix useSendAnswers e Quiz page
    useSendAnswers ora invia le risposte in modo corretto
    La pagina Quiz gestisce l'invio delle risposte
    il pulsante di invio aspetta che tutte le risposte siano state selezionate prima di apparire
    preparata costante quizResult per gestire il risultato del quiz che dovrà essere mostrato in endModule

--- frontend/src/hooks/fetch/useSendAnswers.ts
+++ frontend/src/hooks/fetch/useSendAnswers.ts
@@ -2,34 +2,44 @@ import axios from "axios"
 import { useState } from "react"
 import useToast from "../toast/useToast"
 import { useArgumentContext } from "../../context/argument/useArgumentContext"
+//import { useUserContext } from "../../context/user/useUserContext"
 
-interface Ianswers {
+interface Irequest {
     answers: string[]
     email: string
 }
 
 export function useSendAnswers() {
     const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
     const { showToast } = useToast()
     const { argument } = useArgumentContext()
-
+    //const { user } = useUserContext()
     
-    const sendAnswers = async (payload: Ianswers) => {
+    const sendAnswers = async (payload: string[]) => {
         setIsLoading(true)
+        setError(null)
+
+        const request: Irequest = {
+            answers: payload,
+            email: "giovanni@esempio.com"
+            //email: user?.email
+            // per ora è hardcoded perchè non è implementata l'autenticazione
+        }
+
         try {
-            const response = await axios.post(`http://localhost:3000/api/game/${argument}/sendAnswers`, payload)
-            console.log(response.data)
+            const response = await axios.post(`http://localhost:3000/api/game/${argument}/send-answers`, request)
             showToast("Risposte inviate con successo!", "success")
             return response.data
         } catch (error) {
             console.error("Errore durante la richiesta:", error)
+            setError("Errore durante il caricamento delle risposte")
             showToast("Errore durante il caricamento delle risposte", "danger")
             throw error
         } finally {
             setIsLoading(false)
         }
     }
-    /* questo è un esempio di come inviare le risposte
-    await sendAnswers({answers:["<a>", "Promise"], email:"giovanni@esempio.com"})*/
-    return { sendAnswers, isLoading }
+
+    return { sendAnswers, isLoading, error, setIsLoading }
 }
