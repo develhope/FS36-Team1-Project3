38339548c6a7a302cb14e7c791f5616d977f8f58
luciamartino <luciamartino.lm98@gmail.com>
2025-07-29 12:50:32 +0200

    commit new hook

--- backend/src/controllers/usersController.ts
+++ backend/src/controllers/usersController.ts
@@ -36,8 +36,8 @@ export const getAllUsers = async (req: Request, res: Response<User[] | ErrorResp
 export const createNewUserController = async (req: Request, res: Response<User | ErrorResponse>) => {
   try {
     const user = req.body;
-    const {id} = await createNewUser(user);
-    await createNewProgress(id);
+    const result = await createNewUser(user);
+    await createNewProgress(result.id);
     res.json(user);
   } catch (error) {
     handleControllerError(error, res, "Created user successfully");
