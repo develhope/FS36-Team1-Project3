b3cc50e5e71ad52a7e27a7e791178ab34e054234
klevi.smula <klevi.smula@gmail.com>
2025-07-30 10:03:59 +0200

    fixed infinity bug

--- frontend/src/components/QuizProgress.tsx
+++ frontend/src/components/QuizProgress.tsx
@@ -6,13 +6,13 @@ interface QuizProgressProps{
         totalQuestions?: number
 }
 
-const QuizProgress: React.FC<QuizProgressProps> = ({questionIndex, totalQuestions = 0})=>{
-        const percent =  Math.round((questionIndex / totalQuestions) * 100)
+const QuizProgress: React.FC<QuizProgressProps> = ({questionIndex, totalQuestions = 1})=>{
+        const percent = Math.round((questionIndex / totalQuestions) * 100)
         return(
         <>
                 <div className="relative w-40 h-40 flex items-center justify-center">
                         <Circle 
-                        percent={percent}
+                        percent={totalQuestions ? percent: 1}
                         strokeWidth={8}
                         strokeColor="var(--stroke-color-progress-bar)"
                         trailWidth={8}
