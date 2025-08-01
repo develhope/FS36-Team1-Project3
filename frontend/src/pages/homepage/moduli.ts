9fc2cbbe4ceb5bf67858bfd4d1603ee93ec7d1ac
sandro997 <alessandrofavazza97@gmail.com>
2025-08-01 22:23:17 +0200

    rimossa chiave check da oggetto homepage/moduli

--- frontend/src/pages/homepage/moduli.ts
+++ frontend/src/pages/homepage/moduli.ts
@@ -7,56 +7,38 @@ import reactImg from "../../assets/icone/react.png"
 import sqlImg from "../../assets/icone/sql.png"
 import gitImg from "../../assets/icone/git.png"
 import nodeImg from "../../assets/icone/nodejs.png"
-import checkImg from "../../assets/icone/check.jpg"
-import openLockImg from "../../assets/icone/open-lock.png"
-import closedLockImg from "../../assets/icone/closed-lock.png"
 
 export const moduli = [
 	{
 		argomento: "html",
 		img: htmlImg,
-		check: checkImg,
-
 	},
 	{
 		argomento: "css",
 		img: cssImg,
-		check: openLockImg
 	},
 	{
 		argomento: "javascript",
 		img: jsImg,
-		check: closedLockImg,
-
 	},
 	{
 		argomento: "typescript",
 		img: tsImg,
-		check: closedLockImg,
-
 	},
 	{
 		argomento: "react",
-		img: reactImg,
-		check: closedLockImg,
-
+		img: reactImg, 
 	},
 	{
 		argomento: "sql",
 		img: sqlImg,
-		check: closedLockImg,
-
 	},
 	{
 		argomento: "git",
 		img: gitImg,
-		check: closedLockImg,
-
 	},
 	{
 		argomento: "node",
 		img: nodeImg,
-		check: closedLockImg,
-
 	}
 ]
