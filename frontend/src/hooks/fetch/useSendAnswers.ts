cff939988f2347feee5f00a180fe92509ad905a3
sandro997 <alessandrofavazza97@gmail.com>
2025-08-01 10:53:54 +0200

    aggiunto homepage con progress dinamico, refactor useSendAnswers per poter gestire modalità wild

--- frontend/src/hooks/fetch/useSendAnswers.ts
+++ frontend/src/hooks/fetch/useSendAnswers.ts
@@ -5,8 +5,8 @@ import { useArgumentContext } from "../../context/argument/useArgumentContext"
 import { useUserContext } from "../../context/user/useUserContext"
 
 interface Irequest {
-	answers: string[]
-	email: string
+    answers: string[]
+    email: string
 }
 
 export function useSendAnswers() {
@@ -15,48 +15,29 @@ export function useSendAnswers() {
     const { showToast } = useToast()
     const { argument } = useArgumentContext()
     const { user } = useUserContext()
-
+    
     const sendAnswers = async (payload: string[]) => {
         setIsLoading(true)
         setError(null)
 
         const request: Irequest = {
             answers: payload,
-            // email: "giovanni@esempio.com"
             email: user?.email
-            // per ora è hardcoded perchè non è implementata l'autenticazione
         }
 
-	type sendAnswersType = (payload: string[], isWild?: boolean) => Promise<number>
+        try {
+            const response = await axios.post(`http://localhost:3000/api/game/${argument}/send-answers`, request)
+            showToast("Risposte inviate con successo!", "success")
+            return response.data
+        } catch (error) {
+            console.error("Errore durante la richiesta:", error)
+            setError("Errore durante il caricamento delle risposte")
+            showToast("Errore durante il caricamento delle risposte", "danger")
+            throw error
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
-	const sendAnswers: sendAnswersType = async (payload: string[], isWild = false) => {
-		setIsLoading(true)
-		setError(null)
-
-		const request: Irequest = {
-			answers: payload,
-			email: "giovanni@esempio.com",
-			//email: user?.email
-			// per ora è hardcoded perchè non è implementata l'autenticazione
-		}
-
-		try {
-			//ottimizzazione per non fargli leggere l'intera funzione
-			const endpoint = !isWild
-				? `game/${argument}/send-answers`
-				: `wild/${argument}/send-wild-answers`
-			const response = await axios.post(`http://localhost:3000/api/${endpoint}`, request)
-			showToast("Risposte inviate con successo!", "success")
-			return response.data
-		} catch (error) {
-			console.error("Errore durante la richiesta:", error)
-			setError("Errore durante il caricamento delle risposte")
-			showToast("Errore durante il caricamento delle risposte", "danger")
-			throw error
-		} finally {
-			setIsLoading(false)
-		}
-	}
-
-	return { sendAnswers, isLoading, error, setIsLoading }
+    return { sendAnswers, isLoading, error, setIsLoading }
 }
